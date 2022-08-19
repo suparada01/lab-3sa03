485b6cb83047f78679c5443388a14c6f6a991d77
Suparada Nontasri <6310110487@psu.ac.th>
2022-08-20 03:14:44 +0700

    part 6

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-export default function CharacterCard(props) {
-    const [active, setActive] = useState(false);
-    const activate = () => {
-        if(!active) {
-        setActive(true)
-        props.activationHandler(props.value)
-        }
+const CharacterCard = ({ value, activationHandler, attempt }) => {
+  const [active, setActive] = useState(false);
+  const activate = () => {
+    setActive(true);
+    activationHandler(value);
+  };
+  const className = `card ${active ? "activeCard" : ""}`;
+
+  const attemptRef = useRef(attempt);
+
+  useEffect(() => {
+    if (attemptRef.current !== attempt) {
+      setActive(false);
+      attemptRef.current = attempt;
     }
-    const className = `card ${active ? 'activeCard': ''}`
-    
-    return (
-        <div className={className} onClick={activate}>{props.value}</div>
-    )
+  }, [attempt]);
 
-}
+  return (
+    <div className={className} onClick={activate}>
+      {value}
+    </div>
+  );
+};
+
+export default CharacterCard;
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,19 +1,90 @@
-import React from 'react';
-import CharacterCard from './CharacterCard';
+import React from "react";
+import CharacterCard from "./CharacterCard";
+import * as _ from "lodash";
+import { useState } from "react";
+import { Fireworks } from 'fireworks-js';
+import { useEffect } from "react";
+import { GrPowerReset } from "react-icons/gr";
 
-export default function WordCard(props){
-    const activationHandler = c => { 
-        console.log(`${c} has been activated.`) 
+const prepareStateFromWord = (given_word) => {
+  let word = given_word.toUpperCase();
+  let chars = _.shuffle(Array.from(word));
+  return {
+    word,
+    chars,
+    attempt: 1,
+    guess: "",
+    completed: false,
+  };
+};
+
+const WordCard = ({ value }) => {
+  const [state, setState] = useState(prepareStateFromWord(value));
+  const [isShowReset, setIsShowReset] = useState(false);
+  const [text, setText] = useState("");
+
+  const activationHandler = (c) => {
+    console.log(`${c} has been activated.`);
+
+    let guess = state.guess + c;
+    setState({ ...state, guess });
+    if (guess.length === state.word.length) {
+      if (guess === state.word) {
+        setText("congratulations!, You won.");
+        Fireworks.start()
+        setState({ ...state, guess: "", completed: true });
+      } else {
+        setText("that answer is incorrect !!!, Please try again.");
+        setState({
+          ...state,
+          guess: "",
+          attempt: state.attempt + 1,
+          completed: false,
+        });
+      }
     }
+  };
 
-    return (
-        <div>
-            { 
-                Array.from(props.value).map((c, i) => 
-                    <CharacterCard value={c} key={i} activationHandler={activationHandler}/>
-                )
-            }
- </div>
-    )
- 
-}
+  const handleReset = () => {
+    window.location.reload(false);
+  };
+
+  useEffect(() => {
+    if (state.completed) {
+      const timer = setTimeout(() => {
+        setIsShowReset(true);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [state.completed]);
+  return (
+    <div>
+      {state.completed ? (
+        <h1
+          style={{ color: "red", textTransform: "uppercase", opacity: "0.8" }}
+        >
+          {text}
+        </h1>
+      ) : (
+        <h1
+          style={{ color: "black", textTransform: "uppercase", opacity: "0.8" }}
+        >
+          {text}
+        </h1>
+      )}
+      {state.chars.map((c, i) => (
+        <CharacterCard
+          value={c}
+          key={i}
+          activationHandler={activationHandler}
+          attempt={state.attempt}
+        />
+      ))}
+      {isShowReset && (
+        <GrPowerReset onClick={handleReset} style={{ cursor: "pointer" }} />
+      )}
+    </div>
+  );
+};
+
+export default WordCard;
